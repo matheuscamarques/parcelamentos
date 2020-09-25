574ed11de98b26156a1d18ef6e7bb58f224bd908
matheuscamarques <37029621+matheuscamarques@users.noreply.github.com>
2020-09-25 11:45:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php 
 
-//phpinfo();
-$ip = getenv("REMOTE_ADDR");
-
-
-
-
-echo get_client_ip();
-echo $ip;
-
-
-function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
-
-
+require_once "./interface/";
 ?>
 
